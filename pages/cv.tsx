15b09f5f2187978bdd6b1f484d0080653440d620
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2020-12-25 02:27:54 +0200

    convert cv yaml to json

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,18 +1,20 @@
 import fs from 'fs'
 import path from 'path'
+const yaml = require('js-yaml')
 
 export default function Cv({response}) {
+  console.log(response)
   return (<>
-    <p>cv: {response.cv}</p>
+    <p>cv: {response.cvFilepath}</p>
   </>)
 }
 
 export async function getStaticProps() {
-  const cvFilepath = path.join(process.cwd(), 'data', 'cv.yaml')
-  const cv = fs.readFileSync(cvFilepath, 'utf-8')
   let response: {[key: string]: any} = {}
+  const cvFilepath = path.join(process.cwd(), 'data', 'cv.yaml')
+  const cvYaml = fs.readFileSync(cvFilepath, 'utf-8')
+  const cv = yaml.safeLoad(cvYaml)
 
-  response.cvFilepath = cvFilepath
   response.cv = cv
   console.log(response)
 
