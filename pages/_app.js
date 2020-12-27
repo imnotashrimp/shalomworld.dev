77d2cbb2b9953717f5e66faab1f8f863c51aea4d
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2020-12-27 08:08:34 +0200

    add font awesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+    <Head>
+      <script src="https://kit.fontawesome.com/c070445439.js" crossorigin="anonymous"></script>
+    </Head>
+    <Component {...pageProps} />
+  </>)
 }
 
 export default MyApp
