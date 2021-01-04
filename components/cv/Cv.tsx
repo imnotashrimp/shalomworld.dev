4c7ad949e8aa2ef555a4b3775670a3caf952c9ec
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2021-01-04 07:53:32 +0200

    componentize cv

---
+++ components/cv/Cv.tsx
@@ -0,0 +1,34 @@
+import styles from '../../styles/Cv.module.css'
+import CvHeader from './CvHeader'
+import ExperienceEntry from './ExperienceEntry'
+import SkillSection from './SkillSection'
+
+export default function Cv(props) {
+  const {
+    frontmatter,
+    companies,
+    workExperience,
+    skills,
+    communities,
+    awards,
+    training
+  } = props.data
+
+  return(<div className={styles.cv}>
+    <CvHeader frontmatter = {frontmatter} />
+
+    <h3>Work Experience</h3>
+    {workExperience.map((position, index) => {
+      return <ExperienceEntry
+        key={index}
+        company={companies[position.companyId]}
+        position={position}
+      />
+    })}
+
+    <SkillSection title="Skills" array={skills} />
+    <SkillSection title="Communities" array={communities} />
+    <SkillSection title="Awards" array={awards} />
+    <SkillSection title="Education and Training" array={training} />
+  </div>)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,30 @@
 import Head from 'next/head'
+import Cv from '../components/cv/Cv'
 import fs from 'fs'
 import path from 'path'
-import styles from '../styles/Cv.module.css'
-import CvHeader from '../components/cv/CvHeader'
-import ExperienceEntry from '../components/cv/ExperienceEntry'
-import SkillSection from '../components/cv/SkillSection'
 const yaml = require('js-yaml')
 
-export default function Cv({response}) {
-  const {
-    frontmatter,
-    companies,
-    workExperience,
-    skills,
-    communities,
-    awards,
-    training
-  } = response.cv
-
-  return(<div className={styles.cv}>
+export default function Home({cvData}) {
+  return (<>
     <Head>
       <title key="title">Stefan (Shalom) Boroda</title>
     </Head>
 
-    <CvHeader frontmatter = {frontmatter} />
-
-    <h3>Work Experience</h3>
-    {workExperience.map((position, index) => {
-      return <ExperienceEntry
-        key={index}
-        company={companies[position.companyId]}
-        position={position}
-      />
-    })}
-
-    <SkillSection title="Skills" array={skills} />
-    <SkillSection title="Communities" array={communities} />
-    <SkillSection title="Awards" array={awards} />
-    <SkillSection title="Education and Training" array={training} />
-  </div>)
+    <Cv data={cvData.cv}/>
+  </>)
 }
 
 export async function getStaticProps() {
-  let response: {[key: string]: any} = {}
+  let cvData: {[key: string]: any} = {}
   const cvFilepath = path.join(process.cwd(), 'data', 'cv.yaml')
   const cvYaml = fs.readFileSync(cvFilepath, 'utf-8')
   const cv = yaml.safeLoad(cvYaml)
 
-  response.cv = cv
+  cvData.cv = cv
 
   return {
     props: {
-      response,
+      cvData,
     }, // passed to the page component as props
   }
 }
