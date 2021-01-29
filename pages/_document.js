d337f0a09787cb375931017b0074374516d82ddb
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2021-01-29 09:28:50 +0200

    indent ga embedded script

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-let googleTagManagerSrc
-let gaEmbeddedScript
+let googleTagManagerSrc,
+    gaEmbeddedScript
 
 if (process.env.NODE_ENV === 'production') {
   googleTagManagerSrc = 'https://www.googletagmanager.com/gtag/js?id=G-E77ZNRKHH1'
   gaEmbeddedScript = `
-  window.dataLayer = window.dataLayer || []
-  function gtag() {dataLayer.push(arguments)}
-  gtag('js', new Date());
-  gtag('config', 'G-E77ZNRKHH1')
+    window.dataLayer = window.dataLayer || []
+    function gtag() {dataLayer.push(arguments)}
+    gtag('js', new Date());
+    gtag('config', 'G-E77ZNRKHH1')
   `
 }
 
