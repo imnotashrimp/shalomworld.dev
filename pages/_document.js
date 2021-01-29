dd52f44e77ca925be15459368a9b2602fc7d671b
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2021-01-29 02:16:49 +0200

    add google tag manager tags

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+let googleTagManagerSrc
+let gaEmbeddedScript
+
+if (process.env.NODE_ENV === 'production') {
+  googleTagManagerSrc = 'https://www.googletagmanager.com/gtag/js?id=G-E77ZNRKHH1'
+  gaEmbeddedScript = `
+  window.dataLayer = window.dataLayer || []
+  function gtag() {dataLayer.push(arguments)}
+  gtag('js', new Date());
+  gtag('config', 'G-E77ZNRKHH1')
+  `
+}
+
+class MyDocument extends Document {
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async src={googleTagManagerSrc} />
+          <script dangerouslySetInnerHTML={{__html: gaEmbeddedScript}} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
